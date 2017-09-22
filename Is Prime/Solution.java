dc34bf96c4a0989418aca9d3bb5d6dfcbbc8c4f7
105400 <105400@DESKTOP-TP6SVH3>
2017-09-22 23:39:45 +1000

    Signed-off-by: 105400 <105400@DESKTOP-TP6SVH3>

---
+++ Is Prime/Solution.java
@@ -0,0 +1,23 @@
+package ocpjp;
+
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+   public static void main(String[] args) {
+      Scanner in = new Scanner(System.in);
+      BigInteger n = in.nextBigInteger();
+      in.close();
+      // Write your code here.
+       if(isPrime(n)) System.out.println("prime");
+       else System.out.println("not prime");
+   }
+    
+    public static Boolean isPrime(BigInteger n){
+        return n.isProbablePrime(1);
+    }
+}
